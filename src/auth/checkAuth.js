7ad9d920bde542caa99b54b7d83efa9b31ef542d
Le Tien <tienle21032001@gmail.com>
2024-08-10 00:40:23 +0700

    check permission

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,53 @@
+const { findById } = require("../services/apiKey.service");
+
+const HEADER = {
+    API_KEY: 'x-api-key',
+    AUTHORIZATION: 'authorization'
+}
+
+const apiKey = async (req, res, next) => {
+    try {
+        const key = req.headers[HEADER.API_KEY]?.toString();
+        if (!key) {
+            return res.status(403).json({
+                message: 'Forbidden Error'
+            });
+        }
+
+        // check objKey
+        const objKey = await findById(key)
+        if (!objKey) {
+            return res.status(403).json({
+                message: 'Forbidden Error'
+            });
+        }
+
+        req.objKey = objKey;
+        return next();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const permission = (permission) => {
+    return (req, res, next) => {
+        if (!req.objKey.permissions) {
+            return res.status(403).json({
+                message: 'Permission denied'
+            });
+        }
+        console.log('permission::', req.objKey.permissions);
+        const validPermissions = req.objKey.permissions.includes(permission);
+        if (!validPermissions) {
+            return res.status(403).json({
+                message: 'Permission denied'
+            });
+        }
+        return next();
+    }
+}
+
+module.exports = {
+    apiKey,
+    permission
+}
