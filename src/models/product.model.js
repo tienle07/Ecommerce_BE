2e490269d682b1d78d43be0131c8f633943ebd6d
Le Tien <tienle21032001@gmail.com>
2024-08-12 10:43:06 +0700

    add product model

---
+++ src/models/product.model.js
@@ -0,0 +1,84 @@
+"use strict";
+
+const { Schema, model } = require("mongoose");
+
+const DOCUMENT_NAME = "Product";
+const COLLECTION_NAME = "Products";
+
+const productSchema = new Schema(
+    {
+        product_name: {
+            type: String,
+            required: true,
+        },
+        product_thump: {
+            type: String,
+            required: true,
+        },
+        product_description: String,
+        product_price: {
+            type: Number,
+            required: true,
+        },
+        product_quantity: {
+            type: Number,
+            required: true,
+        },
+        product_type: {
+            type: String,
+            required: true,
+            enum: ['Electronics', 'Clothing', 'Furniture']
+        },
+        product_shop: {
+            type: Schema.Types.ObjectId, ref: 'Shop'
+        },
+        product_attributes: {
+            type: Schema.Types.Mixed,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+        collection: COLLECTION_NAME,
+    }
+);
+
+// define the product type = clothing
+const clothingSchema = new Schema(
+    {
+        brand: {
+            type: String,
+            required: true,
+        },
+        size: String,
+        material: String,
+    },
+    {
+        timestamps: true,
+        collection: "clothes",
+    }
+);
+
+//define the product type = electronic
+const electronicSchema = new Schema(
+    {
+        manufacturer: {
+            type: String,
+            required: true,
+        },
+        model: String,
+        color: String,
+    },
+    {
+        timestamps: true,
+        collection: "electronics",
+    }
+);
+
+module.exports = {
+    product: model(DOCUMENT_NAME, productSchema),
+    electronic: model('Electronics', electronicSchema),
+    clothing: model('Clothing', clothingSchema)
+}
+
+
