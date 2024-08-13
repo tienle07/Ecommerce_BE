c33c175b934c8ae7a1e1d6129facbf8ba1378126
Le Tien <tienle21032001@gmail.com>
2024-08-13 09:03:03 +0700

    fix product model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -42,7 +42,7 @@ const productSchema = new Schema(
             default: 4.5,
             min: [1, 'Rating must be above 1.0'],
             max: [5, 'Rating must be above 5.0'],
-            set: (val) => Math.round(value * 10) / 10
+            set: (val) => Math.round(val * 10) / 10
         },
         product_variations: {
             type: Array,
