d1c5e0af756dc10abd577cd9c51e6d52bf8d95d1
Le Tien <tienle21032001@gmail.com>
2024-08-13 08:29:25 +0700

    optimize add product

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -52,6 +52,10 @@ const clothingSchema = new Schema(
         },
         size: String,
         material: String,
+        product_shop: {
+            type: Schema.Types.ObjectId,
+            ref: 'Shop'
+        }
     },
     {
         timestamps: true,
@@ -79,10 +83,31 @@ const electronicSchema = new Schema(
     }
 );
 
+const furnitureSchema = new Schema(
+    {
+        brand: {
+            type: String,
+            required: true,
+        },
+        size: String,
+        material: String,
+        product_shop: {
+            type: Schema.Types.ObjectId,
+            ref: 'Shop'
+        }
+    },
+    {
+        timestamps: true,
+        collection: "furnitures",
+    }
+);
+
 module.exports = {
     product: model(DOCUMENT_NAME, productSchema),
     electronic: model('Electronics', electronicSchema),
-    clothing: model('Clothing', clothingSchema)
+    clothing: model('Clothing', clothingSchema),
+    furniture: model('Furniture', furnitureSchema),
+
 }
 
 
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,19 +1,20 @@
 'use strict';
 
 const { BadRequestError } = require('../core/error.response');
-const { product, clothing, electronic } = require('../models/product.model');
+const { product, clothing, electronic, furniture } = require('../models/product.model');
 
 class ProductFactory {
 
+    static productRegistry = {}
+
+    static registerProductType(type, classRef) {
+        ProductFactory.productRegistry[type] = classRef;
+    }
+
     static async createProduct(type, payload) {
-        switch (type) {
-            case 'Electronics':
-                return new Electronics(payload).createProduct();
-            case 'Clothing':
-                return new Clothing(payload).createProduct();
-            default:
-                throw new BadRequestError('Invalid Product Types ' + type)
-        }
+        const productClass = ProductFactory.productRegistry[type];
+        if (!productClass) throw new BadRequestError('Invalid Product Types ' + type);
+        return new productClass(payload).createProduct();
     }
 }
 
@@ -71,5 +72,25 @@ class Electronics extends Product {
         return newProduct;
     }
 }
+
+class Furniture extends Product {
+    async createProduct() {
+        const newFurniture = await furniture.create({
+            ...this.product_attributes,
+            product_shop: this.product_shop
+        })
+        if (!newFurniture) throw new BadRequestError('create new Furniture  error');
+
+        const newProduct = await super.createProduct(newFurniture._id);
+        if (!newProduct) throw new BadRequestError('create new Product error');
+
+        return newProduct;
+    }
+}
+
+// register product types
+ProductFactory.registerProductType('Electronics', Electronics);
+ProductFactory.registerProductType('Clothing', Clothing);
+ProductFactory.registerProductType('Furniture', Furniture);
 
 module.exports = ProductFactory;
